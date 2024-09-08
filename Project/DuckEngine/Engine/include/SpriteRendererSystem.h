e62618d3f7e965e959546b12da74ce7e4377cdd0
lucaslol <61584821+lucasyee999@users.noreply.github.com>
2024-09-09 00:09:18 +0800

    add definition in cpp

--- Project/DuckEngine/Engine/include/SpriteRendererSystem.h
+++ Project/DuckEngine/Engine/include/SpriteRendererSystem.h
@@ -13,16 +13,6 @@
 class DUCKENGINE_API SpriteRendererSystem : public System
 {
 public:
-	void Update() override
-	{
-		for (const auto& [entityId, spriteRenderer] : DuckEngine::DUCKENGINE_ComponentManager.GetComponents<SpriteRendererComponent>())
-		{
-			SpriteRendererComponent* spriteRenderer = DuckEngine::DUCKENGINE_ComponentManager.GetComponent<SpriteRendererComponent>(entityId);
-			TransformComponent* transform = DuckEngine::DUCKENGINE_ComponentManager.GetComponent<TransformComponent>(entityId);
-			if (spriteRenderer->sprite && transform)
-			{
-				std::cout << "SpriteRenderer: " << transform->x << " " << transform->y << " \n";
-			}
-		}
-	}
+	void Update() override;
+
 };
