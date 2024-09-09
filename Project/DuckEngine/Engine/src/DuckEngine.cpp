e377e6327f088a5256a02e28cf77bb9205fb693a
Zikry <112854745+DigiZik@users.noreply.github.com>
2024-09-09 19:45:28 +0800

    Update DuckEngine.cpp

--- Project/DuckEngine/Engine/src/DuckEngine.cpp
+++ Project/DuckEngine/Engine/src/DuckEngine.cpp
@@ -14,8 +14,8 @@
 #include "SpriteRendererSystem.h"
 
 #include "imgui.h"
-#include "backends/imgui_impl_opengl3.h"
-#include "backends/imgui_impl_glfw.h"
+#include "imgui_impl_opengl3.h"
+#include "imgui_impl_glfw.h"
 
 
 //GraphicsManager graphicsManager;
