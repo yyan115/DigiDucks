b0af8bc2f49ef484cc4a67d3466dca9d6245c9e7
yankaitan <yankai.tan@digipen.edu>
2024-09-20 09:44:31 +0800

    add file & serialiaztion files

---
+++ Project/DuckEngine/Engine/DuckEngine/File.cpp
@@ -0,0 +1,63 @@
+#include "File.h"
+#include <iostream>
+#include <string.h>
+#include <algorithm>
+
+namespace
+{
+	char convertCharacters(char d)
+	{
+		if (d == '/')
+			return '\\';
+		else
+			return (char)tolower(d);
+	}
+
+	inline void toLower(std::string& s)
+	{
+		std::transform(s.begin(), s.end(), s.begin(), convertCharacters);
+	}
+}
+
+namespace Framework
+{
+
+	FilePath::FilePath()
+	{
+
+	}
+
+	FilePath::FilePath(std::string file)
+	{
+		SetFilePath(file);
+	}
+
+	FilePath& FilePath::operator=(std::string file)
+	{
+		SetFilePath(file);
+		return *this;
+	}
+
+	void FilePath::SetFilePath(std::string file)
+	{
+		toLower(file);
+
+		FullPath = file;
+
+		std::size_t dirEnd = file.find_last_of("\\/");
+		dirEnd = (dirEnd == std::string::npos) ? 0 : dirEnd + 1;
+
+		std::size_t fileEnd = file.find_last_of(".");
+		fileEnd = (fileEnd == std::string::npos) ? file.size() : fileEnd;
+
+		//Extension may be nothing
+		Extension = file.substr(fileEnd);
+		FileName = file.substr(dirEnd, fileEnd - dirEnd);
+		FullDirectory = file.substr(0, dirEnd);
+	}
+
+	std::string FilePath::GetFilePathWithNewExtension(const std::string& newExtension)
+	{
+		return FullDirectory + FileName + newExtension;
+	}
+}
