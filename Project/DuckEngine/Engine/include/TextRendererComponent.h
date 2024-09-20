929ed6e7650747864ed7d09c61d6c874cc20f0b2
Jovan <kaiovanchua@gmail.com>
2024-09-20 16:02:49 +0800

    textrendercomponent

---
+++ Project/DuckEngine/Engine/include/TextRendererComponent.h
@@ -0,0 +1,27 @@
+#pragma once
+#include "Component.h"
+#include "string"
+#include "Sprite.h"
+#include "ft2build.h"
+
+// Export/Import macro
+#ifdef DUCKENGINE_EXPORTS
+#define DUCKENGINE_API __declspec(dllexport)
+#else
+#define DUCKENGINE_API __declspec(dllimport)
+#endif
+
+typedef unsigned int Text;
+
+class DUCKENGINE_API TextRendererComponent : public Component
+{
+public:
+    std::string text;
+    std::string fontPath;
+    int fontSize;
+    Text texture;  // store rendered glyphs as textures
+
+    TextRendererComponent(const std::string& text, const std::string& fontPath, int fontSize = 48) // for eg
+        : text(text), fontPath(fontPath), fontSize(fontSize) {}
+
+};
