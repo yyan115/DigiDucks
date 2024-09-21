42e79f63ecd236af590ecc083e0766fb29059988
sTsenre <h.yonghengernest@digipen.edu>
2024-09-21 23:41:42 +0800

    Fix Merge Errors

--- Project/DuckEngine/Engine/include/Bounding.h
+++ Project/DuckEngine/Engine/include/Bounding.h
@@ -67,7 +67,6 @@ public:
 };
 
 
-class DUCKENGINE_API BoundingBox : private BoundingCollider {
 class DUCKENGINE_API BoundingBox : private BoundingCollider, public Component {
 private:
 	Vec2 size{};
@@ -135,7 +134,6 @@ public:
 };
 
 
-class DUCKENGINE_API BoundingCircle : private BoundingCollider {
 class DUCKENGINE_API BoundingCircle : private BoundingCollider, public Component {
 private:
 	float radius{};
