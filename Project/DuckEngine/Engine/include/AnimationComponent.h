fbf44e87c7d619aef6e762f75db8f193cc46184b
Lucas <lucasyee999@gmail.com>
2024-09-24 15:40:01 +0800

    edit animation component and add system

--- Project/DuckEngine/Engine/include/AnimationComponent.h
+++ Project/DuckEngine/Engine/include/AnimationComponent.h
@@ -12,32 +12,30 @@
 #endif
 
 
-class Frame {
-public:
-    int x, y, width, height; // Position and size of the frame in the sprite sheet
-    float duration; // Duration for which this frame should be displayed
-
-    Frame(int x, int y, int width, int height, float duration)
-        : x(x), y(y), width(width), height(height), duration(duration) {}
-};
+//class Frame {
+//public:
+//    int x, y, width, height; // Position and size of the frame in the sprite sheet
+//    float duration; // Duration for which this frame should be displayed
+//
+//    Frame(int x, int y, int width, int height, float duration)
+//        : x(x), y(y), width(width), height(height), duration(duration) {}
+//};
 
 class Animation {
 private:
-    std::vector<Frame> Frames;  // List of frames for the animation
     int currentFrame;      // Keeps track of the current frame
     float frameTimer;
     float totalDuration;        // animation cycle
 
 public:
+    std::vector<Texture> Frames;  // List of frames for the animation
     Animation() : currentFrame(0), frameTimer(0.0f), totalDuration(0.0f) {}
 };
 
 class DUCKENGINE_API AnimationComponent : public Component
 {
-private:
-    Animation* currentAnimation;
-
 public:
+    Animation* currentAnimation;
     AnimationComponent() : currentAnimation(nullptr) {}
     std::shared_ptr<Component> Clone() const override
     {
