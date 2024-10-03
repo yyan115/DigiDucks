1780f774e1b51469a8b25d88dbf3b51425ef43db
DigiZik <112854745+DigiZik@users.noreply.github.com>
2024-10-03 14:45:23 +0800

    Moved editor to engine temp
    fix dependency issue

--- Project/DuckEngine/Editor/main.cpp
+++ Project/DuckEngine/Editor/main.cpp
@@ -1,40 +1,40 @@
-#include "DuckEngine.h"
-#include "LoggerManager.h"
-
-static DuckEngine engine;
-
-
-int main(void)
-{
-    _CrtSetDbgFlag(_CRTDBG_ALLOC_MEM_DF | _CRTDBG_LEAK_CHECK_DF);
-
-    engine.Initialize();
-    DUCKLOG_INFO("Engine initialized.");
-
-    try {
-        DUCKLOG_INFO("Game Started.");
-        while (engine.Running())
-        {
-
-            engine.Update();
-
-            engine.StartDraw();
-
-            engine.Draw();
-
-            engine.EndDraw();
-
-            //THROW_EXCEPTION("Test Error");
-        }
-    }
-    catch (const DetailedException& ex) {
-        DUCKLOG_CRASH(ex);
-    }
-    std::cout << "Exited!\n";
-
-    engine.Exit();
-
-    //cleanup();
-
-    return 0;
-}
+//#include "DuckEngine.h"
+//#include "LoggerManager.h"
+//
+//static DuckEngine engine;
+//
+//
+//int main(void)
+//{
+//    _CrtSetDbgFlag(_CRTDBG_ALLOC_MEM_DF | _CRTDBG_LEAK_CHECK_DF);
+//
+//    engine.Initialize();
+//    DUCKLOG_INFO("Engine initialized.");
+//
+//    try {
+//        DUCKLOG_INFO("Game Started.");
+//        while (engine.Running())
+//        {
+//
+//            engine.Update();
+//
+//            engine.StartDraw();
+//
+//            engine.Draw();
+//
+//            engine.EndDraw();
+//
+//            //THROW_EXCEPTION("Test Error");
+//        }
+//    }
+//    catch (const DetailedException& ex) {
+//        DUCKLOG_CRASH(ex);
+//    }
+//    std::cout << "Exited!\n";
+//
+//    engine.Exit();
+//
+//    //cleanup();
+//
+//    return 0;
+//}
