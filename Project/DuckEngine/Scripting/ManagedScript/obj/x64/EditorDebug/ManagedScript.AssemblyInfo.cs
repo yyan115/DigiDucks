01cf4f6a1509d407fdc17a2bbffb7f9269b2287e
lucaslol <61584821+lucasyee999@users.noreply.github.com>
2024-11-06 21:20:51 +0800

    add comments

--- Project/DuckEngine/Scripting/ManagedScript/obj/x64/EditorDebug/ManagedScript.AssemblyInfo.cs
+++ Project/DuckEngine/Scripting/ManagedScript/obj/x64/EditorDebug/ManagedScript.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ManagedScript")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("EditorDebug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6cdc2ebeeef4aaedb82c0aaafe689340ab817423")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0607bee7bba91b67f856db3529672bb314f3c449")]
 [assembly: System.Reflection.AssemblyProductAttribute("ManagedScript")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ManagedScript")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
