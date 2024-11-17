034640335c918254f8ce9764b6c8d6c8e3595461
sTsenre <h.yonghengernest@digipen.edu>
2024-11-17 18:03:04 +0800

    Enable Multicore For Building
    Added Layering on Game Scene

--- Project/DuckEngine/Scripting/ManagedScript/obj/x64/EditorDebug/ManagedScript.AssemblyInfo.cs
+++ Project/DuckEngine/Scripting/ManagedScript/obj/x64/EditorDebug/ManagedScript.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ManagedScript")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("EditorDebug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+009e122a6dee2f480401345d1b164b5b84e0c2d5")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4284616adf1131e5bbb4fef5f9bd4bbb6274a078")]
 [assembly: System.Reflection.AssemblyProductAttribute("ManagedScript")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ManagedScript")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
