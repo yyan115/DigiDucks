82262b23b567ca2f1eaa8fe8c52e0b85465348ca
sTsenre <h.yonghengernest@digipen.edu>
2024-11-29 16:56:32 +0800

    Set Layer Order

--- Project/DuckEngine/Game/src/PlayerLogic.cpp
+++ Project/DuckEngine/Game/src/PlayerLogic.cpp
@@ -360,7 +360,7 @@ Entity* PlayerLogic::makeObject(ItemType type)
 	Entity* newObject = nullptr;
 	newObject = DuckEngine::DUCKENGINE_EntityFactory.CreateEntity(circleCollider->getCenter() + offSet, Vec2{ 1.5f, 1.5f });
 	SpriteRendererComponent* spriteRenderer = DuckEngine::DUCKENGINE_ComponentManager.AddComponent<SpriteRendererComponent>(newObject->entityID, true);
-	spriteRenderer->sortingOrder = 2;
+	spriteRenderer->sortingOrder = 3;
 
 	switch (type)
 	{
