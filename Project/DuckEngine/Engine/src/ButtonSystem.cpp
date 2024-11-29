7e62815b442a741000b58bba3f616e21969c1714
Lucas <lucasyee999@gmail.com>
2024-11-29 18:13:49 +0800

    Update ButtonSystem.cpp

--- Project/DuckEngine/Engine/src/ButtonSystem.cpp
+++ Project/DuckEngine/Engine/src/ButtonSystem.cpp
@@ -88,6 +88,7 @@ void ButtonSystem::Update()
                     {
                         button->onClick();
                     }
+                    button->onHover();
                 }
                 else {
 
