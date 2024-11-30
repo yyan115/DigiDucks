bac2ac517779f750d32ac26ff09fe7fae53bde37
lucaslol <61584821+lucasyee999@users.noreply.github.com>
2024-11-30 15:51:48 +0800

    attempt fix combine logic

--- Project/DuckEngine/Game/src/CombineLogic.cpp
+++ Project/DuckEngine/Game/src/CombineLogic.cpp
@@ -106,330 +106,276 @@ bool canCombine(ItemType lhs, ItemType rhs)
 *
 * @return the combined item
 * ****************************************************************/
-std::pair<int, ItemType> combineObjects(std::pair<int, ItemType> lhs, std::pair<int, ItemType> rhs)
-{
-    DuckEngine::DUCKENGINE_EntityManager.RemoveEntity(rhs.first);
-    ItemType newItem = ItemType::EMPTY;
-    int ObjectID = lhs.first;
-	SpriteRendererComponent* sprite = DuckEngine::DUCKENGINE_ComponentManager.GetComponent<SpriteRendererComponent>(ObjectID);
-    if (!sprite)
-    {
-        std::cout << "NO SPRITE\n";
+std::pair<int, ItemType> combineObjects(std::pair<int, ItemType> lhs, std::pair<int, ItemType> rhs) {
+    auto transform = DuckEngine::DUCKENGINE_ComponentManager.GetComponent<TransformComponent>(lhs.first);
+    if (!transform) {
+        std::cerr << "Error: No transform component on first object\n";
         return std::pair<int, ItemType>();
     }
 
-    // Define the possible combinations for lhs
-    switch (lhs.second)
-    {
-        // Empty plates can combine with initial ingredients
+    Vector2D position = transform->GetPosition();
+    Vector2D scale = transform->scale;
+
+    Entity* newEntity = &DuckEngine::DUCKENGINE_EntityManager.CreateEntity();
+    auto newTransform = DuckEngine::DUCKENGINE_ComponentManager.AddComponent<TransformComponent>(newEntity->entityID);
+    auto sprite = DuckEngine::DUCKENGINE_ComponentManager.AddComponent<SpriteRendererComponent>(newEntity->entityID, true);
+
+    newTransform->SetPosition(position);
+    newTransform->scale = scale;
+    sprite->sortingOrder = 3;
+
+    ItemType newItem = ItemType::EMPTY;
+
+    switch (lhs.second) {
     case ItemType::GREY_PLATE:
-        if (rhs.second == ItemType::BUN) 
-        {
-            sprite->texture = AssetManager::GetTextureByName("bun_plate");
-			newItem = ItemType::BUN_PLATE;
-            break;
-		}
-		else if (rhs.second == ItemType::C_CHEESE) 
-        {
-			sprite->texture = AssetManager::GetTextureByName("cheese_plate");
-			newItem = ItemType::CHEESE_PLATE;
-			break;
-		} 
-		else if (rhs.second == ItemType::C_PATTY) 
-        {
-			sprite->texture = AssetManager::GetTextureByName("patty_plate");
-			newItem = ItemType::PATTY_PLATE;
-			break;
-		}
-        break;
-
-    case ItemType::WHITE_PLATE:
-		if (rhs.second == ItemType::C_LETTUCE) 
-        {
-			sprite->texture = AssetManager::GetTextureByName("lettuce_plate");
-			newItem = ItemType::LETTUCE_PLATE;
-			break;
-		}
-		else if (rhs.second == ItemType::C_TOMATO) 
-        {
-			sprite->texture = AssetManager::GetTextureByName("tomato_plate");
-			newItem = ItemType::TOMATO_PLATE;
-			break;
-		}
-		else if (rhs.second == ItemType::C_SHRIMP) 
-        {
-			sprite->texture = AssetManager::GetTextureByName("shrimp_plate");
-			newItem = ItemType::SHRIMP_PLATE;
-			break;
-		}
-        break;
-
-        // BURGER INGREDIENTS
-        // Single Ingredient combine with empty or partial plates
-    case ItemType::BUN:
-        if (rhs.second == ItemType::GREY_PLATE)
-        {
+        if (rhs.second == ItemType::BUN) {
             sprite->texture = AssetManager::GetTextureByName("bun_plate");
             newItem = ItemType::BUN_PLATE;
         }
-        else if (rhs.second == ItemType::CHEESE_PLATE)
-        {
+        else if (rhs.second == ItemType::C_CHEESE) {
+            sprite->texture = AssetManager::GetTextureByName("cheese_plate");
+            newItem = ItemType::CHEESE_PLATE;
+        }
+        else if (rhs.second == ItemType::C_PATTY) {
+            sprite->texture = AssetManager::GetTextureByName("patty_plate");
+            newItem = ItemType::PATTY_PLATE;
+        }
+        break;
+
+    case ItemType::WHITE_PLATE:
+        if (rhs.second == ItemType::C_LETTUCE) {
+            sprite->texture = AssetManager::GetTextureByName("lettuce_plate");
+            newItem = ItemType::LETTUCE_PLATE;
+        }
+        else if (rhs.second == ItemType::C_TOMATO) {
+            sprite->texture = AssetManager::GetTextureByName("tomato_plate");
+            newItem = ItemType::TOMATO_PLATE;
+        }
+        else if (rhs.second == ItemType::C_SHRIMP) {
+            sprite->texture = AssetManager::GetTextureByName("shrimp_plate");
+            newItem = ItemType::SHRIMP_PLATE;
+        }
+        break;
+
+    case ItemType::BUN:
+        if (rhs.second == ItemType::GREY_PLATE) {
+            sprite->texture = AssetManager::GetTextureByName("bun_plate");
+            newItem = ItemType::BUN_PLATE;
+        }
+        else if (rhs.second == ItemType::CHEESE_PLATE) {
             sprite->texture = AssetManager::GetTextureByName("bun_cheese_plate");
             newItem = ItemType::BUN_CHEESE_PLATE;
         }
-        else if (rhs.second == ItemType::PATTY_PLATE)
-        {
+        else if (rhs.second == ItemType::PATTY_PLATE) {
             sprite->texture = AssetManager::GetTextureByName("burger_plate");
             newItem = ItemType::BURGER_PLATE;
         }
-        else if (rhs.second == ItemType::CHEESE_PATTY_PLATE)
-        {
+        else if (rhs.second == ItemType::CHEESE_PATTY_PLATE) {
             sprite->texture = AssetManager::GetTextureByName("cheese_burger_plate");
             newItem = ItemType::CHEESE_BURGER_PLATE;
         }
         break;
 
     case ItemType::C_CHEESE:
-        if (rhs.second == ItemType::GREY_PLATE)
-        {
+        if (rhs.second == ItemType::GREY_PLATE) {
             sprite->texture = AssetManager::GetTextureByName("cheese_plate");
             newItem = ItemType::CHEESE_PLATE;
         }
-        else if (rhs.second == ItemType::BUN_PLATE)
-        {
+        else if (rhs.second == ItemType::BUN_PLATE) {
             sprite->texture = AssetManager::GetTextureByName("bun_cheese_plate");
             newItem = ItemType::BUN_CHEESE_PLATE;
         }
-        else if (rhs.second == ItemType::PATTY_PLATE)
-        {
+        else if (rhs.second == ItemType::PATTY_PLATE) {
             sprite->texture = AssetManager::GetTextureByName("cheese_patty_plate");
             newItem = ItemType::CHEESE_PATTY_PLATE;
         }
-        else if (rhs.second == ItemType::BURGER_PLATE)
-        {
+        else if (rhs.second == ItemType::BURGER_PLATE) {
             sprite->texture = AssetManager::GetTextureByName("cheese_burger_plate");
             newItem = ItemType::CHEESE_BURGER_PLATE;
         }
         break;
 
     case ItemType::C_PATTY:
-        if (rhs.second == ItemType::GREY_PLATE)
-        {
+        if (rhs.second == ItemType::GREY_PLATE) {
             sprite->texture = AssetManager::GetTextureByName("patty_plate");
             newItem = ItemType::PATTY_PLATE;
         }
-        else if (rhs.second == ItemType::BUN_PLATE)
-        {
+        else if (rhs.second == ItemType::BUN_PLATE) {
             sprite->texture = AssetManager::GetTextureByName("burger_plate");
             newItem = ItemType::BURGER_PLATE;
         }
-        else if (rhs.second == ItemType::CHEESE_PLATE)
-        {
+        else if (rhs.second == ItemType::CHEESE_PLATE) {
             sprite->texture = AssetManager::GetTextureByName("cheese_patty_plate");
             newItem = ItemType::CHEESE_PATTY_PLATE;
         }
-        else if (rhs.second == ItemType::BUN_CHEESE_PLATE)
-        {
+        else if (rhs.second == ItemType::BUN_CHEESE_PLATE) {
             sprite->texture = AssetManager::GetTextureByName("cheese_burger_plate");
             newItem = ItemType::CHEESE_BURGER_PLATE;
         }
-		break;
+        break;
 
         // Partial plate combine with single ingredients
     case ItemType::BUN_PLATE:
-        if (rhs.second == ItemType::C_CHEESE)
-        {
+        if (rhs.second == ItemType::C_CHEESE) {
             sprite->texture = AssetManager::GetTextureByName("bun_cheese_plate");
             newItem = ItemType::BUN_CHEESE_PLATE;
         }
-        else if (rhs.second == ItemType::C_PATTY)
-        {
+        else if (rhs.second == ItemType::C_PATTY) {
             sprite->texture = AssetManager::GetTextureByName("burger_plate");
             newItem = ItemType::BURGER_PLATE;
         }
         break;
 
     case ItemType::CHEESE_PLATE:
-        if (rhs.second == ItemType::BUN)
-        {
+        if (rhs.second == ItemType::BUN) {
             sprite->texture = AssetManager::GetTextureByName("bun_cheese_plate");
             newItem = ItemType::BUN_CHEESE_PLATE;
         }
-        else if (rhs.second == ItemType::C_PATTY)
-        {
+        else if (rhs.second == ItemType::C_PATTY) {
             sprite->texture = AssetManager::GetTextureByName("cheese_patty_plate");
             newItem = ItemType::CHEESE_PATTY_PLATE;
         }
         break;
 
     case ItemType::PATTY_PLATE:
-        if (rhs.second == ItemType::BUN)
-        {
+        if (rhs.second == ItemType::BUN) {
             sprite->texture = AssetManager::GetTextureByName("burger_plate");
             newItem = ItemType::BURGER_PLATE;
         }
-        else if (rhs.second == ItemType::C_CHEESE)
-        {
+        else if (rhs.second == ItemType::C_CHEESE) {
             sprite->texture = AssetManager::GetTextureByName("cheese_patty_plate");
             newItem = ItemType::CHEESE_PATTY_PLATE;
         }
         break;
 
     case ItemType::BUN_CHEESE_PLATE:
-        if (rhs.second == ItemType::C_PATTY)
-        {
+        if (rhs.second == ItemType::C_PATTY) {
             sprite->texture = AssetManager::GetTextureByName("cheese_burger_plate");
             newItem = ItemType::CHEESE_BURGER_PLATE;
         }
         break;
 
     case ItemType::BURGER_PLATE:
-        if (rhs.second == ItemType::C_CHEESE) 
-        {
+        if (rhs.second == ItemType::C_CHEESE) {
             sprite->texture = AssetManager::GetTextureByName("cheese_burger_plate");
             newItem = ItemType::CHEESE_BURGER_PLATE;
         }
         break;
 
     case ItemType::CHEESE_PATTY_PLATE:
-        if (rhs.second == ItemType::BUN) 
-        {
+        if (rhs.second == ItemType::BUN) {
             sprite->texture = AssetManager::GetTextureByName("cheese_burger_plate");
             newItem = ItemType::CHEESE_BURGER_PLATE;
         }
         break;
 
-
         // SALAD INGREDIENTS
-        // Single ingredients can combine with empty or partial plates
     case ItemType::C_LETTUCE:
-        if (rhs.second == ItemType::WHITE_PLATE)
-        {
+        if (rhs.second == ItemType::WHITE_PLATE) {
             sprite->texture = AssetManager::GetTextureByName("lettuce_plate");
             newItem = ItemType::LETTUCE_PLATE;
         }
-        else if (rhs.second == ItemType::TOMATO_PLATE)
-        {
+        else if (rhs.second == ItemType::TOMATO_PLATE) {
             sprite->texture = AssetManager::GetTextureByName("lettuce_tomato_plate");
             newItem = ItemType::LETTUCE_TOMATO_PLATE;
         }
-        else if (rhs.second == ItemType::SHRIMP_PLATE)
-        {
+        else if (rhs.second == ItemType::SHRIMP_PLATE) {
             sprite->texture = AssetManager::GetTextureByName("lettuce_shrimp_plate");
             newItem = ItemType::LETTUCE_SHRIMP_PLATE;
         }
-        else if (rhs.second == ItemType::TOMATO_SHRIMP_PLATE)
-        {
+        else if (rhs.second == ItemType::TOMATO_SHRIMP_PLATE) {
             sprite->texture = AssetManager::GetTextureByName("salad_plate");
             newItem = ItemType::SALAD_PLATE;
         }
         break;
 
     case ItemType::C_TOMATO:
-        if (rhs.second == ItemType::WHITE_PLATE)
-        {
+        if (rhs.second == ItemType::WHITE_PLATE) {
             sprite->texture = AssetManager::GetTextureByName("tomato_plate");
             newItem = ItemType::TOMATO_PLATE;
         }
-        else if (rhs.second == ItemType::LETTUCE_PLATE)
-        {
+        else if (rhs.second == ItemType::LETTUCE_PLATE) {
             sprite->texture = AssetManager::GetTextureByName("lettuce_tomato_plate");
             newItem = ItemType::LETTUCE_TOMATO_PLATE;
         }
-        else if (rhs.second == ItemType::SHRIMP_PLATE)
-        {
+        else if (rhs.second == ItemType::SHRIMP_PLATE) {
             sprite->texture = AssetManager::GetTextureByName("tomato_shrimp_plate");
             newItem = ItemType::TOMATO_SHRIMP_PLATE;
         }
-        else if (rhs.second == ItemType::LETTUCE_SHRIMP_PLATE)
-        {
+        else if (rhs.second == ItemType::LETTUCE_SHRIMP_PLATE) {
             sprite->texture = AssetManager::GetTextureByName("salad_plate");
             newItem = ItemType::SALAD_PLATE;
         }
-		break;
+        break;
 
     case ItemType::C_SHRIMP:
-		if (rhs.second == ItemType::WHITE_PLATE)
-		{
-			sprite->texture = AssetManager::GetTextureByName("shrimp_plate");
-			newItem = ItemType::SHRIMP_PLATE;
-		}
-		else if (rhs.second == ItemType::LETTUCE_PLATE)
-		{
-			sprite->texture = AssetManager::GetTextureByName("lettuce_shrimp_plate");
-			newItem = ItemType::LETTUCE_SHRIMP_PLATE;
-		}
-		else if (rhs.second == ItemType::TOMATO_PLATE)
-		{
-			sprite->texture = AssetManager::GetTextureByName("tomato_shrimp_plate");
-			newItem = ItemType::TOMATO_SHRIMP_PLATE;
-		}
-		else if (rhs.second == ItemType::LETTUCE_TOMATO_PLATE)
-		{
-			sprite->texture = AssetManager::GetTextureByName("salad_plate");
-			newItem = ItemType::SALAD_PLATE;
-		}
-	    break;
+        if (rhs.second == ItemType::WHITE_PLATE) {
+            sprite->texture = AssetManager::GetTextureByName("shrimp_plate");
+            newItem = ItemType::SHRIMP_PLATE;
+        }
+        else if (rhs.second == ItemType::LETTUCE_PLATE) {
+            sprite->texture = AssetManager::GetTextureByName("lettuce_shrimp_plate");
+            newItem = ItemType::LETTUCE_SHRIMP_PLATE;
+        }
+        else if (rhs.second == ItemType::TOMATO_PLATE) {
+            sprite->texture = AssetManager::GetTextureByName("tomato_shrimp_plate");
+            newItem = ItemType::TOMATO_SHRIMP_PLATE;
+        }
+        else if (rhs.second == ItemType::LETTUCE_TOMATO_PLATE) {
+            sprite->texture = AssetManager::GetTextureByName("salad_plate");
+            newItem = ItemType::SALAD_PLATE;
+        }
+        break;
 
-        // Partial plate combine with single ingredients
     case ItemType::LETTUCE_PLATE:
-        if (rhs.second == ItemType::C_TOMATO)
-        {
+        if (rhs.second == ItemType::C_TOMATO) {
             sprite->texture = AssetManager::GetTextureByName("lettuce_tomato_plate");
             newItem = ItemType::LETTUCE_TOMATO_PLATE;
         }
-        else if (rhs.second == ItemType::C_SHRIMP)
-        {
+        else if (rhs.second == ItemType::C_SHRIMP) {
             sprite->texture = AssetManager::GetTextureByName("lettuce_shrimp_plate");
             newItem = ItemType::LETTUCE_SHRIMP_PLATE;
         }
         break;
 
     case ItemType::TOMATO_PLATE:
-        if (rhs.second == ItemType::C_LETTUCE)
-        {
+        if (rhs.second == ItemType::C_LETTUCE) {
             sprite->texture = AssetManager::GetTextureByName("lettuce_tomato_plate");
             newItem = ItemType::LETTUCE_TOMATO_PLATE;
         }
-        else if (rhs.second == ItemType::C_SHRIMP)
-        {
+        else if (rhs.second == ItemType::C_SHRIMP) {
             sprite->texture = AssetManager::GetTextureByName("tomato_shrimp_plate");
             newItem = ItemType::TOMATO_SHRIMP_PLATE;
         }
-		break;
+        break;
 
     case ItemType::SHRIMP_PLATE:
-        if (rhs.second == ItemType::C_LETTUCE)
-        {
+        if (rhs.second == ItemType::C_LETTUCE) {
             sprite->texture = AssetManager::GetTextureByName("lettuce_shrimp_plate");
             newItem = ItemType::LETTUCE_SHRIMP_PLATE;
         }
-        else if (rhs.second == ItemType::C_TOMATO)
-        {
+        else if (rhs.second == ItemType::C_TOMATO) {
             sprite->texture = AssetManager::GetTextureByName("tomato_shrimp_plate");
             newItem = ItemType::TOMATO_SHRIMP_PLATE;
         }
-		break;
+        break;
 
     case ItemType::LETTUCE_TOMATO_PLATE:
-        if (rhs.second == ItemType::C_SHRIMP) 
-        {
+        if (rhs.second == ItemType::C_SHRIMP) {
             sprite->texture = AssetManager::GetTextureByName("salad_plate");
             newItem = ItemType::SALAD_PLATE;
         }
-		break;
+        break;
 
     case ItemType::LETTUCE_SHRIMP_PLATE:
-        if (rhs.second == ItemType::C_TOMATO) 
-        {
+        if (rhs.second == ItemType::C_TOMATO) {
             sprite->texture = AssetManager::GetTextureByName("salad_plate");
             newItem = ItemType::SALAD_PLATE;
         }
         break;
 
     case ItemType::TOMATO_SHRIMP_PLATE:
-        if (rhs.second == ItemType::C_LETTUCE) 
-        {
+        if (rhs.second == ItemType::C_LETTUCE) {
             sprite->texture = AssetManager::GetTextureByName("salad_plate");
             newItem = ItemType::SALAD_PLATE;
         }
@@ -439,5 +385,13 @@ std::pair<int, ItemType> combineObjects(std::pair<int, ItemType> lhs, std::pair<
         break;
     }
 
-    return std::make_pair(ObjectID, newItem);
+    if (newItem != ItemType::EMPTY) {
+        DuckEngine::DUCKENGINE_EntityManager.RemoveEntity(lhs.first);
+        DuckEngine::DUCKENGINE_EntityManager.RemoveEntity(rhs.first);
+        return std::make_pair(newEntity->entityID, newItem);
+    }
+    else {
+        DuckEngine::DUCKENGINE_EntityManager.RemoveEntity(newEntity->entityID);
+        return std::pair<int, ItemType>();
+    }
 }
