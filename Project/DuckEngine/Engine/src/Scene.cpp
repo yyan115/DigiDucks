a20d87deee4af98d43bb9ff4ad23cb93f87328c1
lucaslol <61584821+lucasyee999@users.noreply.github.com>
2025-02-04 22:10:30 +0800

    remove unused header

--- Project/DuckEngine/Engine/src/Scene.cpp
+++ Project/DuckEngine/Engine/src/Scene.cpp
@@ -18,7 +18,6 @@ written consent of DigiPen Institute of Technology is prohibited.
 #include "Scene.h"
 #include "DuckEngine.h"
 #include "SoundSystem.h"
-#include "GameLogicManager.h"
 
 /**************************************************************************
 @brief Loads resources and initializes the scene.
