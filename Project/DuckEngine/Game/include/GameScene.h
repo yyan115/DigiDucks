03a1d2bb630a9dd6f98df4a24da1f657c80e7204
Jovan <kaiovanchua@gmail.com>
2025-02-07 00:42:18 +0800

    documentation

--- Project/DuckEngine/Game/include/GameScene.h
+++ Project/DuckEngine/Game/include/GameScene.h
@@ -1,8 +1,12 @@
 /******************************************************************************/
 /*!
 \file       GameScene.h
-\author     Ernest Ho, h.yonghengernest, 2301223
+\author     Ernest Ho, h.yonghengernest, 2301223 (50%)
 \par        h.yonghengernestt@digipen.edu
+\author     Jovan Chua, c.shengkaijovan, 2301244 (25%)
+\par        c.shengkaijovan@digipen.edu
+\author     Tan Yan Kai, yankai.tan, 2301312 (25%)
+\par        yankai.tan@digipen.edu
 \date       November 18 2024
 \brief      Definition of the GameScene class, which handles the game logic
             for the main game scene. This includes loading assets, initializing
