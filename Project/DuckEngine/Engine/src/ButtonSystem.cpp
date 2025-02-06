bb7ce528a8236f40a272a4a5a9f9f28d342c5196
tobenwarrior <lucasyee999@gmail.com>
2025-02-06 13:32:22 +0800

    fix button can click even if not visible

--- Project/DuckEngine/Engine/src/ButtonSystem.cpp
+++ Project/DuckEngine/Engine/src/ButtonSystem.cpp
@@ -66,6 +66,11 @@ void ButtonSystem::Render()
             continue;
         }
 
+        if (!DuckEngine::DUCKENGINE_ComponentManager.GetComponent<SpriteRendererComponent>(entityId)->isVisible)
+        {
+            continue;
+        }
+
 		if (DuckEngine::DUCKENGINE_EntityManager.ShouldSkipDueToInvisibleAncestor(entityId)) 
         {
 			continue;
