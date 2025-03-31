f46912454a024e1511cc03305f95cffcfb823cbb
sTsenre <h.yonghengernest@digipen.edu>
2025-04-01 00:52:39 +0800

    Fix Layer issue

--- Project/DuckEngine/Game/src/PlayerLogic.cpp
+++ Project/DuckEngine/Game/src/PlayerLogic.cpp
@@ -875,7 +875,7 @@ Entity* PlayerLogic::makeObject(ItemType type)
 	Entity* newObject = nullptr;
 	newObject = DuckEngine::DUCKENGINE_EntityFactory.CreateEntity(circleCollider->getCenter() + offSet, Vec2{ 1.5f, 1.5f });
 	SpriteRendererComponent* spriteRenderer = DuckEngine::DUCKENGINE_ComponentManager.AddComponent<SpriteRendererComponent>(newObject->entityID, true);
-	spriteRenderer->sortingOrder = 11;
+	spriteRenderer->sortingOrder = 19;
 
 	spriteRenderer->texture = AssetManager::GetTextureByName(whatType(type));
 
